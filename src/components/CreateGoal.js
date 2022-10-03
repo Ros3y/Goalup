13adf163f9c3db82a54bd11044f8abcb2f94f9d4
Zach Rowe <zachrowe.personal@gmail.com>
2022-10-02 19:28:02 -0600

    switch class to className for cancel button

--- src/components/CreateGoal.js
+++ src/components/CreateGoal.js
@@ -28,7 +28,7 @@ function CreateGoal(props) {
           <div style={{ width: "36px", height: "36px" }} />
           <h2>Create a Goal</h2>
           <button onClick={() => props.onCancel()}>
-            <span class="material-symbols-outlined">cancel</span>
+            <span className="material-symbols-outlined">cancel</span>
           </button>
         </div>
         <br></br>
